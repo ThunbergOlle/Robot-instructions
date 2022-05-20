059c22d83114f372572c25d108d781246e1981ec
emil.a.grahn <emil.a.grahn@elev.hassleholm.se>
2022-05-20 13:02:06 +0200

    changed library to prompts

--- src/PlayerInput/PlayerInput.ts
+++ src/PlayerInput/PlayerInput.ts
@@ -1,9 +1,10 @@
 import { Direction, ReadLine } from 'readline';
 import { Compass, DirectionInput } from '../Utils/interfaces';
+import prompt from 'prompts';
 
 /* Function which parses an input and converts it to numbers */
-export const parseInput = (input: string) =>
-    input
+export const parseInput = (userInput: string) =>
+userInput
         .slice(0, 2)
         .split('')
         .map((char) => Number(char))
@@ -11,20 +12,19 @@ export const parseInput = (input: string) =>
 
 /* Function for asking the user for the grid size */
 export async function AskForGridSize(rl: ReadLine): Promise<{ gridX: number; gridY: number }> {
-    return new Promise((resolve, reject) => {
-        rl.question(
-            'How big field do you wish to set? Please enter two numbers, the first being the width and the second being the height\n',
-            (_input) => {
-                const input: number[] = parseInput(_input);
-
-                if (input.length !== 2) {
-                    rl.write('Please enter correct data\n');
-                    reject();
-                }
-                return resolve({ gridX: input[0], gridY: input[1] });
-            }
-        );
+    const userInput = await prompt({
+        type: 'text',
+        name: "grid",
+        message: 'How big field do you wish to set? Please enter two numbers, the first being the width and the second being the height: \n',
     });
+
+    const input: number[] = parseInput(userInput.grid);
+
+    if (input.length !== 2) {
+        console.log('Please enter correct data');
+        throw 'Please enter correct data';
+    }
+    return { gridX: input[0], gridY: input[1] };
 }
 
 /* Function for asking the user for the starting position and orientation */
@@ -34,42 +34,43 @@ export async function AskForStartingPos(
     gridSizeX: number,
     gridSizeY: number
 ): Promise<{ x: number; y: number; facing: Compass }> {
-    return new Promise((resolve, reject) => {
-        rl.question(
-            'Where do you want to start? Please enter two coordinate values and one character describing the orientation. Valid orientations are: N,E,S,W\n',
-            (response) => {
-                const _input = response.replaceAll(' ', ''); // Remove spaces, if there are any. ES2021 feature
+        const question = await prompt({
+            type: 'text',
+            name: "grid",
+            message: "Where do you want to start? Please enter two coordinate values and one character describing the orientation. Valid orientations are: N,E,S,W\n"})
+                const _input = question.grid.replaceAll(' ', ''); // Remove spaces, if there are any. ES2021 feature
                 const coordinates = _input.substring(0, 2); // The first to characters are coordinates
                 const facing = _input[2]; // The third character is the orientation which the bot is facing.
                 const input: number[] = parseInput(coordinates);
                 if (input.length !== 2 || !facingInputList.includes(facing as Compass)) {
-                    reject(
-                        'Please enter correct data: Allowed inputs are 0-9 & NESW, please enter three characters\n'
-                    );
+                    console.log('Please enter correct data');
+                    throw 'Please enter correct data';
                 }
-                if (input[0] > gridSizeX) return reject('Input X is bigger than the grid size on X');
-                if (input[1] > gridSizeY) return reject('Input Y is bigger than the grid size on Y');
-
-                return resolve({ x: input[0], y: input[1], facing: facing as Compass });
+                if (input[0] > gridSizeX){
+                    console.log('Input X is bigger than the grid size on X');
+                    throw 'Input X is bigger than the grid size on X';
+                }
+                if (input[1] > gridSizeY) {
+                    console.log('Input Y is bigger than the grid size on Y');
+                    throw('Input Y is bigger than the grid size on Y');
+                }
+                return ({ x: input[0], y: input[1], facing: facing as Compass });
             }
-        );
-    });
-}
+        
+ 
+
 /* Function for asking the user for directions */
 const directionInputList: DirectionInput[] = ['L', 'R', 'F'];
 export async function AskForDirections(rl: ReadLine): Promise<DirectionInput[]> {
-    return new Promise((resolve) => {
-        rl.question(
-            'Where do you want the robot to go? You can give directions by a sequence of characters. Valid characters are: \nF: Walk forward\nR: Turn right\nL: Turn left\n',
-            (_input) => {
-                const input = _input
+        const question2 = await prompt({
+            type: 'text',
+            name: "grid",
+            message: 'Where do you want the robot to go? You can give directions by a sequence of characters. Valid characters are: \nF: Walk forward\nR: Turn right\nL: Turn left\n'});
+                const input = question2.grid
                     .replace(' ', '')
                     .split('')
-                    .filter((char) =>
+                    .filter((char: string) =>
                         directionInputList.includes(char as DirectionInput)
                     ) as DirectionInput[];
-                resolve(input);
+                return input;
             }
-        );
-    });
-}
